de5e4055784be93bcda5289b4428e8e9093968ad
Tom Grochowicz <tgrochowicz@gmail.com>
2014-02-09 15:33:13 -0600

    fix copy, gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ var gulp = require('gulp')
 gulp.task('styles', function() {
 	return gulp.src('app/less/main.less')
 		.pipe(less({
-			paths: [ path.join(__dirname, 'app/bower_components/bootstrap/less/') ]
+			paths: [ path.join(__dirname, 'bower_components/bootstrap/less/') ]
 		}))
 		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
 		.pipe(gulp.dest('dist/css'))
